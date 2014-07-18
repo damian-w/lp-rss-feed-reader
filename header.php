dd11a78f85fa3ebed39c24e1d04e5b924ca76eb6
Damian Worsdell <damian.w@iinet.net.au>
2014-07-19 07:19:30 +0800

    Fixing relative links to content

--- header.php
+++ header.php
@@ -2,16 +2,16 @@
 <html>
 	<head>
 		<title>Breaking News - The Western Australian</title>
-		<link rel="stylesheet" type="text/css" href="http://djw.net.au/lp/reset.css"> <!-- "Reset" the CSS -->
-		<link rel="stylesheet" type="text/css" href="http://djw.net.au/lp/style.css"> <!-- Grab the styling -->
+		<link rel="stylesheet" type="text/css" href="http://djw.net.au/lp/the-west-breaking/reset.css"> <!-- "Reset" the CSS -->
+		<link rel="stylesheet" type="text/css" href="http://djw.net.au/lp/the-west-breaking/style.css"> <!-- Grab the styling -->
 	</head>
 
 	<body>
 		<header>
-			<img src="http://djw.net.au/lp/thick_line.png" class="thick-line" width="384" height="5">
-			<img src="http://djw.net.au/lp/the-western-australian-logo.png" class="logo" width="384">
-			<span class="subtitle"><b>Breaking News</b> #am, 18th of July 2014</span>
-			<img src="http://djw.net.au/lp/thick_line.png" class="thick-line" width="384" height="5">
+			<img src="http://djw.net.au/lp/the-west-breaking/thick_line.png" class="thick-line" width="384" height="5">
+			<img src="http://djw.net.au/lp/the-west-breaking/the-western-australian-logo.png" class="logo" width="384">
+			<span class="subtitle"><b>Breaking News</b> as at 6am, 18th of July 2014</span>
+			<img src="http://djw.net.au/lp/the-west-breaking/thick_line.png" class="thick-line" width="384" height="5">
 		</header>
 
 		<section id="content">
