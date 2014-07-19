a0c196f343b6a0cacd39a4185ece2c73dddd727b
Damian Worsdell <damian.w@iinet.net.au>
2014-07-19 17:07:29 +0800

    Creating a dynamic sample
    Updating the /sample/ to match the current design, and to also be
    dynamic as well (practically exactly the same as /edition/)

--- sample/index.php
+++ sample/index.php
@@ -1,35 +1,25 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<title>Breaking News - The Western Australian</title>
-		<link rel="stylesheet" type="text/css" href="http://djw.net.au/lp/the-west-breaking/reset.css"> <!-- "Reset" the CSS -->
-		<link rel="stylesheet" type="text/css" href="http://djw.net.au/lp/the-west-breaking/style.css"> <!-- Grab the styling -->
-	</head>
+<?php
 
-	<body>
-		<header>
-			<img src="http://djw.net.au/lp/the-west-breaking/thick_line.png" class="thick-line" width="384" height="5">
-			<img src="http://djw.net.au/lp/the-west-breaking/the-western-australian-logo.png" class="logo" width="384">
-			<span class="subtitle"><b>Breaking News</b> as at 6am, 18th of July 2014</span>
-			<img src="http://djw.net.au/lp/the-west-breaking/thick_line.png" class="thick-line" width="384" height="5">
-		</header>
+	/*----------------------------------------------------------*\
+	| **** RSS News Feed Reader  for Little Printer by BERG **** |
+	+------------------------------------------------------------+
+	|      Released under the GNU General Public Licence v2      |
+	+------------------------------------------------------------+
+	|   Created by Damian Worsdell           http://djw.net.au/  |
+	\*----------------------------------------------------------*/
+	
+	// Grab all of our lovely functions...
+	require ('../functions.php');
+	
+	// Send the ETag and the Character Set to the BERG Cloud...
+	littleprinterETag();
+	charset();
+	
+	// Create the publication...
+	generateHeader(); // Generate the header HTML
+	generateContent(); // Generate the HTML for the items
+	generateFooter(); // Generate the footer HTML
+	
+	// Publication sent!!
 
-		<section id="content">
-			<div class="item">
-				<span class="title">QLD Family hit by both MH17 and MH370 plane disasters</span>
-				<span class="text">A Queensland family who lost relatives on doomed Malaysia Airlines flight MH370 are in mourning again after losing more family members on MH17 in Ukraine.</span>
-			</div>
-			<div class="item">
-				<span class="title">Scott falters in bid to reel in McIlroy</span>
-				<span class="text">World number one Adam Scott lost his putting touch over the early holes of his British Open second round.</span>
-			</div>
-			<div class="item">
-				<span class="title">Abbott criticises Russia's response to MH17 disaster</span>
-				<span class="text">Prime Minister Tony Abbott is not accepting Russian explanations for the shooting down of Malaysia Airlines flight MH17 and has demanded it not stand in the way of a full inquiry into the tragedy.</span>
-			</div>
-		</section>
-		<footer>
-			from <b>The Western Australian</b> - by Damian Worsdell
-		</footer>
-	</body>
-</html>
+?>
