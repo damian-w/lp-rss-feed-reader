7971b6735e84038fb2b02fcc9c5ef11ea2e48458
Damian Worsdell <damian.w@iinet.net.au>
2014-07-19 17:03:47 +0800

    Removing config points
    We don’t need any user configuration right now.

--- validate_config/index.php
+++ validate_config/index.php
@@ -1,37 +1,16 @@
 <?php
 
+	/*----------------------------------------------------------*\
+	| **** RSS News Feed Reader  for Little Printer by BERG **** |
+	+------------------------------------------------------------+
+	|      Released under the GNU General Public Licence v2      |
+	+------------------------------------------------------------+
+	|   Created by Damian Worsdell           http://djw.net.au/  |
+	\*----------------------------------------------------------*/
+	
+	// Grab all of our lovely functions...
 	require_once('../functions.php');
-
-	if (isset($_POST['config'])) {
-
-		$post = json_decode($_POST['config'], true);
-
-		if (isset($post['time_preference'])) {
-
-			if ($post['time_preference'] == 'new' || $post['time_preference'] == 'old') {
-				yay();
-			} else {
-				fail(2, $post['time_preference']);
-			} 
-
-		} else fail(1, 0);
-
-	} else header('HTTP/1.0 400 Bad Request', true, 400);
-
-	function yay() {
-		$return = array('valid' => 'true');
-		$die = json_encode($return);
-		die($die);
-	}
-
-	function fail($type, $post) {
-		if ($type == 1)
-			$return = array('valid' => 'false', 'errors' => array('No "time_preference" value passed.'));
-		elseif ($type == 2)
-			$return = array('valid' => 'false', 'errors' => array('"time_preference" value invalid. Supplied was '.$post.', should be new or old.'));
-		header('HTTP/1.0 400 Bad Request', true, 400);
-		$die = json_encode($return);
-		die($die);
-	}
+	
+	// My Little News Reader doesn't need any configuration... yet!
 
 ?>
