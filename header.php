a208205abf319e6b0c2e978d668f5c35663abb0a
Damian Worsdell <damian.w@iinet.net.au>
2014-07-19 17:04:41 +0800

    Adding dynamic edition time and date
    Adds a dynamic edition message and the current date into the publication

--- header.php
+++ header.php
@@ -7,10 +7,11 @@
 	</head>
 
 	<body>
+		<?php if (date('H') > 2 && date("H") < 5) { $edition = 'Early Edition'; } else if (date("H") < 12) { $edition = 'Morning Edition'; } else if (date('H') > 11 && date("H") < 18) { $edition = 'Afternoon Edition'; } else if(date('H') > 17) { $edition = 'Evening Edition'; } ?>
 		<header>
 			<img src="http://djw.net.au/lp/the-west-breaking/thick_line.png" class="thick-line" width="384" height="5">
 			<img src="http://djw.net.au/lp/the-west-breaking/the-western-australian-logo.png" class="logo" width="384">
-			<span class="subtitle"><b>Breaking News</b> as at 6am, 18th of July 2014</span>
+			<span class="subtitle"><b>Breaking News</b> <?php echo $edition; ?>, <?php echo date('jS M Y'); ?></span>
 			<img src="http://djw.net.au/lp/the-west-breaking/thick_line.png" class="thick-line" width="384" height="5">
 		</header>
 
