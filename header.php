c421b6c8f0b62442b2cf3131fb94bb9fd3747eab
Damian Worsdell <damian.w@iinet.net.au>
2014-07-19 17:54:19 +0800

    Fixing HTML Syntax

--- header.php
+++ header.php
@@ -10,8 +10,8 @@
 		<?php if (date('H') > 2 && date("H") < 5) { $edition = 'Early Edition'; } else if (date("H") < 12) { $edition = 'Morning Edition'; } else if (date('H') > 11 && date("H") < 18) { $edition = 'Afternoon Edition'; } else if(date('H') > 17) { $edition = 'Evening Edition'; } ?>
 		<header>
 			<img src="http://djw.net.au/lp/the-west-breaking/thick_line.png" class="thick-line" width="384" height="5">
-			<img src="http://djw.net.au/lp/the-west-breaking/the-western-australian-logo.png" class="logo" width="384">
-			<span class="subtitle"><b>Breaking News</b> <?php echo $edition; ?>, <?php echo date('jS M Y'); ?></span>
+			<img src="http://djw.net.au/lp/the-west-breaking/the-western-australian-logo.png" class="logo" width="384" height="150">
+			<span class="subtitle"><b>HEADLINES</b> <?php echo $edition; ?>, <?php echo date('jS M Y'); ?></span>
 			<img src="http://djw.net.au/lp/the-west-breaking/thick_line.png" class="thick-line" width="384" height="5">
 		</header>
 
